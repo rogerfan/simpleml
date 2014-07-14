f67d1e82a0d2e817dd13c3142c2fd3151893b49f
Roger Fan <rogerfan.rf@gmail.com>
2014-07-14 16:58:59 -0500

    Switched from mean to count_nonzero to calculate proportions, as this way is faster.

--- simpleml/metrics.py
+++ simpleml/metrics.py
@@ -6,18 +6,18 @@ import numpy as np
 
 # Purity measures for binary variables.
 def entropy(labels):
-    prop = np.mean(labels)
+    prop = np.count_nonzero(labels) / len(labels)
     if prop == 0 or prop == 1:
         return 0.
     else:
         return -prop*np.log(prop) - (1-prop)*np.log(1-prop)
 
 def gini(labels):
-    prop = np.mean(labels)
+    prop = np.count_nonzero(labels) / len(labels)
     return prop*(1-prop)
 
 def misclass(labels):
-    prop = np.mean(labels)
+    prop = np.count_nonzero(labels) / len(labels)
     if np.round(prop) == 0:
         return prop
     else:
