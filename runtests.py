1f40f5452f23786b79a093238e378b476756802d
Roger Fan <rogerfan.rf@gmail.com>
2014-07-30 15:48:52 -0500

    Added tests of base classes.

--- runtests.py
+++ runtests.py
@@ -1,6 +1,7 @@
 import nose
 
 
-argv =  ['', '--with-coverage', '--cover-package=simpeml', '--cover-erase',
+argv =  ['', '-v',
+         '--with-coverage', '--cover-package=simpeml', '--cover-erase',
          '--cover-tests', '--cover-branches']
 nose.main(argv=argv)
---
+++ tests/test_baseclasses.py
@@ -0,0 +1,44 @@
+import simpleml.baseclasses as bc
+
+
+classes = ['BinaryClassifier', 'BinaryClassifierWithErrors']
+
+
+def test_req_meth():
+    for clsname in classes:
+        cls = getattr(bc, clsname)
+
+        def func(cls):
+            for funcname in cls._req_meth:
+                getattr(cls, funcname)(None)
+        yield func, cls
+
+class NotBinClass:
+    def fit(self): pass
+
+class SimpBinClass(NotBinClass):
+    def classify(self): pass
+
+class SimpBinClassWithErr(SimpBinClass):
+    def train_err(self): pass
+    def test_err(self): pass
+
+class TestSubclassBinClass:
+    def test1(self):
+        assert not issubclass(NotBinClass, bc.BinaryClassifier)
+
+    def test2(self):
+        assert issubclass(SimpBinClass, bc.BinaryClassifier)
+
+    def test3(self):
+        assert issubclass(SimpBinClassWithErr, bc.BinaryClassifier)
+
+class TestSubclassBinClassWithErr:
+    def test1(self):
+        assert not issubclass(NotBinClass, bc.BinaryClassifierWithErrors)
+
+    def test2(self):
+        assert not issubclass(SimpBinClass, bc.BinaryClassifierWithErrors)
+
+    def test3(self):
+        assert issubclass(SimpBinClassWithErr, bc.BinaryClassifierWithErrors)
