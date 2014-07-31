b87b75bd647331d7ae1d75399583db8732932f33
Roger Fan <rogerfan.rf@gmail.com>
2014-07-30 20:57:58 -0500

    Formatting.

--- runtests.py
+++ runtests.py
@@ -1,7 +1,7 @@
 import nose
 
 
-argv =  ['', '-v',
-         '--with-coverage', '--cover-package=simpleml', '--cover-erase',
-         '--cover-tests', '--cover-branches']
+argv = ['', '-v',
+        '--with-coverage', '--cover-package=simpleml', '--cover-erase',
+        '--cover-tests', '--cover-branches']
 nose.main(argv=argv)
