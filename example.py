21836d2a1aa4321a2e7697fa8c9f8b42f0fc9366
Roger Fan <rogerfan.rf@gmail.com>
2014-07-31 14:39:39 -0500

    Modified example to use new API.

--- example.py
+++ example.py
@@ -11,37 +11,24 @@ from simpleml.ensemble import EnsembleBinaryClassifier
 
 # Load data
 x_data = np.loadtxt('x_data.txt')
-y_class = np.loadtxt('y_class.txt', dtype=int)
+y_data = np.loadtxt('y_class.txt', dtype=int)
+
+num = 5
 
 # Using untransformed data
-x_train1 = x_data[:1000]
-y_train1 = y_class[:1000]
-x_train2 = x_data[1000:2000]
-y_train2 = y_class[1000:2000]
-x_train3 = x_data[2000:3000]
-y_train3 = y_class[2000:3000]
-x_train4 = x_data[3000:4000]
-y_train4 = y_class[3000:4000]
+x_train_list = [x_data[i*1000:(i+1)*1000] for i in range(num)]
+y_train_list = [y_data[i*1000:(i+1)*1000] for i in range(num)]
 x_test  = x_data[-1000:]
-y_test  = y_class[-1000:]
+y_test  = y_data[-1000:]
 
-tree1 = DecisionTree(x_train1, y_train1)
-tree2 = DecisionTree(x_train2, y_train2)
-tree3 = DecisionTree(x_train3, y_train3)
-tree4 = DecisionTree(x_train4, y_train4)
-tree1.fit(min_obs_split=5, max_depth=5)
-tree2.fit(min_obs_split=5, max_depth=5)
-tree3.fit(min_obs_split=5, max_depth=5)
-tree4.fit(min_obs_split=5, max_depth=5)
+trees = [DecisionTree(min_obs_split=5, max_depth=5) for i in range(num)]
+for tree, x, y in zip(trees, x_train_list, y_train_list):
+    tree.fit(x, y)
 
 ensemble = EnsembleBinaryClassifier()
-ensemble.add_model(tree1, weight=1)
-ensemble.add_model(tree2, weight=2)
-ensemble.add_model(tree3, weight=2)
-ensemble.add_model(tree4, weight=2)
+for tree in trees:
+    ensemble.add_model(tree, weight=1)
 
 print(np.mean(ensemble.classify(x_test) == y_test))
-print(np.mean(tree1.classify(x_test) == y_test))
-print(np.mean(tree2.classify(x_test) == y_test))
-print(np.mean(tree3.classify(x_test) == y_test))
-print(np.mean(tree4.classify(x_test) == y_test))
+for tree in trees:
+    print(np.mean(tree.classify(x_test) == y_test))
