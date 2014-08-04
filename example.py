43947e327f8e9a93707bd860710440d1b493fa76
Roger Fan <rogerfan.rf@gmail.com>
2014-08-04 16:06:37 -0500

    Changing example to show usage of random forests through the bagging class.

--- example.py
+++ example.py
@@ -1,34 +1,43 @@
 '''
 Example of how to use Decision Trees and PCA in simpleml.
 '''
+import time
 import numpy as np
 
 # %cd C:/Users/g1rxf01/Downloads/New folder/simpleml
 # %cd M:/Libraries/Documents/Code/Python/simpleml
 
 from simpleml.dectree import DecisionTree
-from simpleml.ensemble import EnsembleBinaryClassifier
+from simpleml.ensemble import BaggingBinaryClassifier
 
 # Load data
 x_data = np.loadtxt('x_data.txt')
 y_data = np.loadtxt('y_class.txt', dtype=int)
 
-num = 5
+x_train = x_data[:5000]
+y_train = y_data[:5000]
+x_test  = x_data[-5000:]
+y_test  = y_data[-5000:]
 
-# Using untransformed data
-x_train_list = [x_data[i*1000:(i+1)*1000] for i in range(num)]
-y_train_list = [y_data[i*1000:(i+1)*1000] for i in range(num)]
-x_test  = x_data[-1000:]
-y_test  = y_data[-1000:]
+model_params = {
+    'min_obs_split': 10,
+    'max_depth': 10,
+    'max_features': 7
+}
 
-trees = [DecisionTree(min_obs_split=5, max_depth=5) for i in range(num)]
-for tree, x, y in zip(trees, x_train_list, y_train_list):
-    tree.fit(x, y)
+bag = BaggingBinaryClassifier(DecisionTree, model_params=model_params,
+                              n_models_fit=40, seed=2349634)
+dtree = DecisionTree(min_obs_split=10, max_depth=10)
 
-ensemble = EnsembleBinaryClassifier()
-for tree in trees:
-    ensemble.add_model(tree, weight=1)
+start = time.clock()
+bag.fit(x_train, y_train)
+print(time.clock() - start)
 
-print(np.mean(ensemble.classify(x_test) == y_test))
-for tree in trees:
-    print(np.mean(tree.classify(x_test) == y_test))
+start = time.clock()
+dtree.fit(x_train, y_train)
+dtree.prune(x_test, y_test)
+print(time.clock() - start)
+
+
+print(bag.test_err(x_test, y_test))
+print(dtree.test_err(x_test, y_test))
