caf30602ce56be397399bf09eed3f0a08c4e9af2
Roger Fan <rogerfan.rf@gmail.com>
2014-09-04 15:12:08 -0500

    Starting multilayer perceptron dev in example.

---
+++ examples/multilayer_perceptron.py
@@ -0,0 +1,21 @@
+'''
+Example of how to use Decision Trees and Random Forests.
+'''
+import sys, os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %cd C:/Users/g1rxf01/Downloads/New folder/simpleml/examples
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from simpleml import MultilayerPerceptron
+
+
+np.random.seed(2345)
+num_obs = 500
+
+x = np.random.normal(size=(num_obs, 2))
+e = np.random.normal(1, size=num_obs)
+y = np.logical_and(np.dot(x, [ 2,-3])+e < 4,
+                   np.dot(x, [-3,-1])+e < 3).astype(int)
