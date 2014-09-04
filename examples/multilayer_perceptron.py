b74d6749ffd39d68f8384a739dc053c37ea92545
Roger Fan <rogerfan.rf@gmail.com>
2014-09-04 16:10:38 -0500

    Cleaning up.

--- examples/multilayer_perceptron.py
+++ examples/multilayer_perceptron.py
@@ -9,7 +9,8 @@ import matplotlib.pyplot as plt
 # %cd C:/Users/g1rxf01/Downloads/New folder/simpleml/examples
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-from simpleml import MultilayerPerceptron
+from simpleml.perceptron import Layer
+from simpleml.baseclasses import Function
 
 
 np.random.seed(2345)
@@ -19,3 +20,5 @@ x = np.random.normal(size=(num_obs, 2))
 e = np.random.normal(1, size=num_obs)
 y = np.logical_and(np.dot(x, [ 2,-3])+e < 4,
                    np.dot(x, [-3,-1])+e < 3).astype(int)
+
+
