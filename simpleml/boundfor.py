fdedbaa6392370dc87579c125b50a8096401b18f
Roger Fan <rogerfan.rf@gmail.com>
2015-05-13 16:24:37 -0500

    Many changes and fixes.

--- simpleml/boundfor.py
+++ simpleml/boundfor.py
@@ -51,12 +51,26 @@ class BoundaryTree:
     def addchild(self, position, label):
         newnode = BoundaryTree(position, label, parent=self)
         self.children.append(newnode)
-
-    def dist(self, y, metric_pos=norm):
-        return metric_pos(y, self.position)
+        self.childnum += 1
 
     def query(self, inp, maxchild=5, metric_pos=norm):
-        A = self.children
+        A = self.children[:]
+
+        if self.childnum < maxchild:
+            A.append(self)
+
+        node_positions = np.array([node.position for node in A])
+        distances = metric_pos(node_positions, inp)
+        min_ind = np.argmin(distances)
+        min_node = A[min_ind]
+
+        if min_node is self:
+            return self
+        else:
+            return min_node.query(inp, maxchild=maxchild, metric_pos=metric_pos)
+
+    def oldquery(self, inp, maxchild=5, metric_pos=norm):
+        A = self.children[:]
 
         if self.childnum < maxchild:
             A.append(self)
@@ -64,7 +78,7 @@ class BoundaryTree:
         mindist = np.inf
         minnode = None
         for node in A:
-            nodedist = node.dist(inp, metric_pos=metric_pos)
+            nodedist = metric_pos(inp, node.position)
             if nodedist < mindist:
                 mindist = nodedist
                 minnode = node
@@ -72,7 +86,9 @@ class BoundaryTree:
         if minnode == self:
             return self
         else:
-            return minnode.query(inp)
+            return minnode.query(inp, maxchild=maxchild, metric_pos=metric_pos)
+
+
 
     def train(self, y, label, labthresh=0.1, maxchild=5,
               metric_lab=cat_noteq, metric_pos=norm):
@@ -100,17 +116,21 @@ class BoundaryForest:
     maxchild : int, optional
         Maximum number of children per node (default 5).
     '''
-    params_names = ('metric_pos', 'metric_lab', 'labthresh', 'maxchild')
+    params_names = ('metric_pos', 'metric_lab', 'labthresh', 'maxchild',
+                    'maxtrees')
+    treeparams_names = ('metric_pos', 'metric_lab', 'labthresh', 'maxchild')
 
     def __init__(self, metric_pos=norm, metric_lab=cat_noteq, labthresh=0.1,
-                 maxchild=5):
+                 maxchild=5, maxtrees=np.inf):
 
         self.metric_pos = metric_pos
         self.metric_lab = metric_lab
         self.labthresh = labthresh
         self.maxchild = maxchild
+        self.maxtrees = maxtrees
 
         self.trees = []
+        self.numtrees = 0
 
     @property
     def params(self):
@@ -119,9 +139,17 @@ class BoundaryForest:
             result[name] = getattr(self, name)
         return result
 
+    @property
+    def treeparams(self):
+        result = {}
+        for name in self.treeparams_names:
+            result[name] = getattr(self, name)
+        return result
+
+
     def train_addpoint(self, y, label):
         for bt in self.trees:
-            bt.train(y, label, **self.params)
+            bt.train(y, label, **self.treeparams)
 
         return self
 
@@ -132,14 +160,34 @@ class BoundaryForest:
 
         return self
 
-    def train(self, data, labels):
-        for pos, lab in zip(data, labels):
+    def fit(self, data, labels):
+        for i, (pos, lab) in enumerate(zip(data, labels)):
+            if i == self.maxtrees:
+                break
             self.trees.append(BoundaryTree(pos, lab))
 
         for i, tree in enumerate(self.trees):
             for j, (pos, lab) in enumerate(zip(data, labels)):
                 if i != j:
-                    tree.train(pos, lab, **self.params)
+                    tree.train(pos, lab, **self.treeparams)
 
+        self.numtrees = len(self.trees)
         return self
 
+    def predict_obs(self, pos):
+        distances = np.full(self.numtrees, np.nan)
+        labels = np.full(self.numtrees, np.nan)
+        for i, tree in enumerate(self.trees):
+            nearest_node = tree.query(pos, maxchild=self.maxchild,
+                                      metric_pos=self.metric_pos)
+            distances[i] = self.metric_pos(pos, nearest_node.position)
+            labels[i] = nearest_node.label
+
+        pred = np.sum(labels/distances) / np.sum(1/distances)
+        return pred
+
+    def predict(self, data):
+        if len(data.shape) == 1:
+            return self.predict_obs(data)
+        else:
+            return np.array([self.predict_obs(obs) for obs in data])
